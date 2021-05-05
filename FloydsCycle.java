61c1da60d4790938d2e92039cb9c42f40fed346e
Samuel Jegede <sam.jegede@outlook.com>
2021-05-05 11:10:56 +0100

    fix indentation #2

--- FloydsCycle.java
+++ FloydsCycle.java
@@ -1,36 +1,36 @@
 
 class FloydsCycle {
     public boolean detectLoop(ListNode head) {
-	    ListNode tortoise = head;
-	    ListNode hare = head;
-	    while(hare!=null&&hare.next!=null) {
-	        if(hare.val == tortoise.val) return true; // loop detected
-	        
-	        tortoise=tortoise.next;
-	        hare=hare.next.next;
-	    }
+        ListNode tortoise = head;
+        ListNode hare = head;
+        while(hare!=null&&hare.next!=null) {
+            if(hare.val == tortoise.val) return true; // loop detected
+            
+            tortoise=tortoise.next;
+            hare=hare.next.next;
+        }
         return false;
     }
 
     public void print(ListNode head) {
-    	while (head != null) {
-    		head = head.next;
-    	}
+        while (head != null) {
+            head = head.next;
+        }
     }
     
     public ListNode fill(int[] arr) {
-    	if (arr.length == 0) return null; 
-    	ListNode dummy = new ListNode(arr[0]);
-    	ListNode node = new ListNode(arr[0]);
-    	node = dummy;
-    	int i = 1;
+        if (arr.length == 0) return null; 
+        ListNode dummy = new ListNode(arr[0]);
+        ListNode node = new ListNode(arr[0]);
+        node = dummy;
+        int i = 1;
         
         // fill linkedlist with integer array
-    	while(i < arr.length) {
-	        node.next = new ListNode(arr[i]);
-	        node = node.next;
-	        i++;
-    	}
-	    return dummy;
+        while(i < arr.length) {
+            node.next = new ListNode(arr[i]);
+            node = node.next;
+            i++;
+        }
+        return dummy;
     }
 } 
